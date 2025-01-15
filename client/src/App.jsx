f094c02d57934dd33376e5761450a44daa110e9c
AbhiDoshi2000 <abhidoshi2000@gmail.com>
2025-01-14 19:37:33 -0600

    function for layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,37 @@
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/login";
 import { Routes, Route, Navigate, Outlet, useLocation } from 'react-router-dom'
+import Toaster from "sonner";
+import TaskDetails from "./pages/TaskDetails";
+import Trash from "./pages/Trash";
+import Users from "./pages/Users";
+import Tasks from "./pages/Tasks";
+
+function Layout() {
+  const user = ""
+
+  const location = useLocation()
+
+  return user ? (
+    <div className="w-full h-screen flex flex-col md:flex-row">
+      <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
+        {/* <Sidebar/> */}
+      </div>
+
+      {/* <MobileSidebar/> */}
+
+      <div className="flex-1 overflow-y-auto">
+        {/* <Navbar/> */}
+
+        <div className="p-4 2xl:px-10">
+          <Outlet/>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Navigate to="/log-in" state={{from: location}} repalce/>
+  )
+}
 
 function App() {
   return (
@@ -20,6 +51,8 @@ function App() {
 
         <Route path='/log-in >' element={<Login />} />  
       </Routes>
+
+      <Toaster richColors/> 
     </main>
   );
 }
