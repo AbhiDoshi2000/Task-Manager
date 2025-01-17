f729644878ac39a0ee2b6422d4e6b32bbc31c03c
AbhiDoshi2000 <abhidoshi2000@gmail.com>
2025-01-16 23:11:25 -0600

    right side of login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -13,6 +13,8 @@ const Login = () => {
 
     const navigate = useNavigate();
 
+    const 
+
     useEffect(() => {
         user && navigate("/dashboard")
     }, [user]);
@@ -23,13 +25,25 @@ const Login = () => {
                 {/* left side */}
                 <div className="h-full w-full lg:w-2/3 flex flex-col items-center justfiy-center">
                     <div className='w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justfy-center gap-5 md:gap-y-10 2xl:-mt-20'>
-                        <span className='flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base bordergray-300 text-gray-600'>Manage all your tasks in one place!</span>
+                        <span className='flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base bordergray-300 text-gray-600'>
+                        Manage all your tasks in one place!
+                        </span>
+                        <p className='flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black
+                        text-center text-blue-700'>
+                            <span>Cloud Based</span>
+                            <span>Task Manager</span>
+                        </p>
 
-                    </div>
+                        <div className='cell'>
+                            <div className='circle rotate-in-up-left'></div>
+                        </div>
+                    </div> 
                 </div>
 
                 {/* right side */}
-                <div></div>
+                <div className='w-full md:w-1/3 p-4 md:p-1 flex flex-col jsutify-center items-center'> 
+                    <form onSubmit={}></form>
+                </div>
 
             </div>
         </div>
