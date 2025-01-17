9502e4f741f35bb5a18100cfb66a4d4be75afd3b
AbhiDoshi2000 <abhidoshi2000@gmail.com>
2025-01-17 01:34:32 -0600

    Completed login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useEffect } from 'react';
 import { useForm } from "react-hook-form"
 import { useNavigate } from 'react-router-dom';
+import Textbox from '../components/Textbox';
+import Button from '../components/Button';
 
 const Login = () => {
     const user = "";
@@ -13,14 +15,16 @@ const Login = () => {
 
     const navigate = useNavigate();
 
-    const 
+    const submitHandler = async (data)=> {
+        console.log("submit")
+    }
 
     useEffect(() => {
         user && navigate("/dashboard")
     }, [user]);
 
     return ( 
-        <div className="w-full min-h-screen flex tems-center justify-center flex-col lg:flex-row bg-white">
+        <div className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-white">
             <div className="w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center">
                 {/* left side */}
                 <div className="h-full w-full lg:w-2/3 flex flex-col items-center justfiy-center">
@@ -42,7 +46,54 @@ const Login = () => {
 
                 {/* right side */}
                 <div className='w-full md:w-1/3 p-4 md:p-1 flex flex-col jsutify-center items-center'> 
-                    <form onSubmit={}></form>
+                    <form onSubmit={handleSubmit( submitHandler )}
+                    className='form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-white
+                    px-10 pt-14 pb-14'
+                    >
+                        <div className=''>
+                            <p className='text-blue-600 text-3xl font-bold text-center'>
+                                Welcome back!
+                            </p>
+                            <p className='text-center text-base text-gray-700'>
+                                Keep all your credentials safe.
+                            </p>
+                        </div>
+                        <div className='flex flex-col gap-y-5'>
+                            <Textbox 
+
+                            placeholder="email@example.com"
+                            type="email"
+                            name="email"
+                            label="Email Address"
+                            className="w-full rounded-full"
+                            register={register("email", {
+                                required: "Email Address is required!",
+                            })}
+                            error={errors.email ? errors.email.message : ""}
+                            />
+                            <Textbox 
+                            placeholder="password"
+                            type="password"
+                            name="password"
+                            label="Password"
+                            className="w-full rounded-full"
+                            register={register("email", {
+                                required: "Password is required!",
+                            })}
+                            error={errors.password ? errors.password.message : ""}
+                            />
+
+                            <span className='text-sm- text-gray-500 hover:text-blue-600 hover:underline cursor-pointer'>
+                                Forgot Password?
+                            </span>
+
+                            <Button
+                            type='submit'
+                            label='Submit'
+                            className='w-full h-10 bg-blue-700 text-white rounded-full' 
+                            />
+                        </div>
+                    </form>
                 </div>
 
             </div>
