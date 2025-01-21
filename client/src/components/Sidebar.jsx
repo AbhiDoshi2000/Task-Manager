0ea87ab14a4f5730ce57e2c9639b01b6d7f3201f
AbhiDoshi2000 <abhidoshi2000@gmail.com>
2025-01-20 18:09:37 -0600

    sidebar nav links and settings button

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -8,8 +8,9 @@ import {
 } from "react-icons/md";
 import { FaTasks, FaTrashAlt, FaUsers } from "react-icons/fa";
 import { useDispatch, useSelector } from 'react-redux';
-import { NavLink, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { setOpenSideBar } from '../redux/slices/authSlice'; 
+import clsx from 'clsx';
 
 const linkData = [
     {
@@ -57,10 +58,28 @@ const Sidebar = () => {
 
     const path = location.pathname.split("/")[1];
 
-    const sidebarLinks = user?.isAdmin ? linkData : linkData.slice(0, 5);
+    const sidebarLinks = user?.role === "Admin" ? linkData : linkData.slice(0, 5);
     
     const closeSidebar = () => {
         dispatch (setOpenSideBar(false));
+    };
+
+    const SidebarNavLink = ({ el }) => {
+        return (
+            <Link 
+                to={el.link} 
+                onClick={closeSidebar}
+                className={clsx (
+                    "w-full lg:w-3/4 flex gap-2 px-3 py-2 rounded-full items-center text-gray-800" + 
+                    "text-base hover:bg-[#2564ed2d]",
+                    path === el.link.split("/")[0] ? "bg-blue-700 text-neutral-100" : ""
+                )}
+            >
+
+                {el.icon}
+                <span className='hover:text-[#2564ed]'>{el.label}</span>
+            </Link>
+        );
     };
 
     return ( 
@@ -75,9 +94,15 @@ const Sidebar = () => {
             <div className='flex-1 flex flex-col gap-y-5 py-8'>
                 {
                     sidebarLinks.map((link) => (
-                        <NavLink el={link} key={link.label}/>
+                        <SidebarNavLink el={link} key={link.label}/>
                     ))
                 }
+            </div>
+            <div className=''>
+                <button className='w-full flex gap-2 p-2 items-center text-lg text-gray-800'>
+                    <MdSettings />
+                    <span>Settings</span>
+                </button>
             </div>
         </div>
     );
