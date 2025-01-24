b678257cc9819ead0af9b1983d1544a8c504d546
AbhiDoshi2000 <abhidoshi2000@gmail.com>
2025-01-23 20:53:11 -0600

    User table done

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -12,7 +12,7 @@ import moment from 'moment';
 import { summary } from '../assets/data';
 import clsx from 'clsx';
 import Chart from '../components/Chart';
-import { PRIOTITYSTYELS, TASK_TYPE, BGS } from '../utils';
+import { PRIOTITYSTYELS, TASK_TYPE, BGS, getInitials } from '../utils';
 import UserInfo from '../components/UserInfo';
 
 
@@ -69,6 +69,12 @@ const TaskTable = ({tasks}) => {
             </div>
           ))}
         </div>
+      </td>
+
+      <td className='py-2 hidden md:block'>
+          <span className='text-base text-gray-600'>
+            {moment(task?.date).fromNow()}
+          </span>
       </td>  
     </tr>
   );
@@ -88,6 +94,59 @@ const TaskTable = ({tasks}) => {
   );
 };
 
+const UserTable = ({ users }) => {
+  const TableHeader = () => (
+    <thead className='border-b border-gray-300'>
+      <tr className='text-black text-left'>
+        <th className='py-2'>Full Name</th>
+        <th className='py-2'>Status</th>
+        <th className='py-2'>Created At</th>
+      </tr>
+    </thead>
+  );
+
+  const TableRow = ({ user }) => (
+    <tr className='border-b border-gray-200 text-gray-600 hover:bg-gray-400/10'>
+      <td className='py-2'>
+        <div className='flex items-center gap-3'>
+          <div className='w-9 h-9 rounded-full text-white flex items-center justify-center text-sm bg-violet-700'>
+            <span className='text-center'>{getInitials(user?.name)}</span>
+          </div>
+
+          <div>
+            <p>{user.name}</p>
+            <span className='text-xs text-black'>{user?.role}</span>
+          </div>
+        </div>
+      </td>
+
+      <td>
+        <p
+          className={clsx(
+            "w-fit px-3 py-1 roudned-full text-sm",
+            user?.isActive ? "bg-blue-200" : "bg-yellow-100"
+          )}
+        >
+          {user?.isActive ? "Active" : "Disabled"}
+        </p>
+      </td>
+      <td className='py-2 text-sm'>{moment(user?.createdAt).fromNow()}</td>
+    </tr>
+  );
+
+  return (
+    <div className='w-full md:w-1/3 bg-white h-fit px-2 md:px-6 py-4 shadow-md rounded'>
+      <table className='w-full mb-5'>
+        <TableHeader />
+        <tbody>
+          {users?.map((user, index) => (
+            <TableRow key ={index + user?._id} user={user} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
 const Dashboard = () => {
   const totals = summary.tasks  
 
@@ -160,9 +219,12 @@ const Dashboard = () => {
 
       <div className='w-full flex flex-col md:flex-row gap-4 2xl:gap-10 py-8'>
           {/* left */}
-          <TaskTable 
-            tasks={summary.last10Task}/>
+          
+          <TaskTable tasks={summary.last10Task}/>
+          
           {/* right */}
+          
+          <UserTable users={summary.users} />
       </div>
     </div>
   );
