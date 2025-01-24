6906b6471c3799eb855fcbe1f95d605bcea1ea08
AbhiDoshi2000 <abhidoshi2000@gmail.com>
2025-01-23 19:46:47 -0600

    User info card on task table

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -12,6 +12,9 @@ import moment from 'moment';
 import { summary } from '../assets/data';
 import clsx from 'clsx';
 import Chart from '../components/Chart';
+import { PRIOTITYSTYELS, TASK_TYPE, BGS } from '../utils';
+import UserInfo from '../components/UserInfo';
+
 
 const TaskTable = ({tasks}) => {
   const ICONS = {
@@ -31,18 +34,44 @@ const TaskTable = ({tasks}) => {
     </thead>
   );
 
-  const TableRow = ({ task })=> <tr className='border-b border-gray-300 text-gray-600 hover:bg-gray-300/10'>
-    <td className='py-2'>
-      <div className='flex items-center gap-2'>
-        <div
-          className={clsx("w-4 h-4 roudned-full", TASK_TYPE[task.stage])}
-        />
+  const TableRow = ({ task })=> (
+    <tr className='border-b border-gray-300 text-gray-600 hover:bg-gray-300/10'>
+      <td className='py-2'>
+        <div className='flex items-center gap-2'>
+          <div
+            className={clsx("w-4 h-4 roudned-full", TASK_TYPE[task.stage])}
+          />
 
-        
-      </div>
-    </td>
-  
-  </tr>;
+          <p className='text-base text-black'>{task.title}</p>
+        </div>
+      </td>
+
+      <td className='py-2'>
+        <div className='flex gap-1 items-center'>
+          <span className={clsx("text-lg", PRIOTITYSTYELS[task.priority])}>
+            {ICONS[task.priority]}
+          </span>
+          <span className='capitalize'>{task.priority}</span>
+        </div>
+      </td>
+
+      <td className='py-2'>
+        <div className='flex'>
+          {task.team.map((m, index) => (
+            <div
+              key={index}
+              className={clsx("w-7 h-7 rounded-full text-white flex items-center justify-center text-sm mr-1", 
+                BGS[index % BGS.length]      
+              )}
+            >
+              
+              <UserInfo user={m} />
+            </div>
+          ))}
+        </div>
+      </td>  
+    </tr>
+  );
   return (
     <>
       <div className='w-full md:w-2/3 bg-white px-2 md:px-4 pt-4 pb-4 shadow-md rounded'>
