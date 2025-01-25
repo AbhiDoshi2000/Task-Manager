10e3c7e887b4e2155dc386cf720861280102b70f
AbhiDoshi2000 <abhidoshi2000@gmail.com>
2025-01-24 23:31:51 -0600

    started tasks page structuring

--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+const TABS = [
+  { title: "Board View", icon: <MdGridView />},
+  { title: "List View", icon: <FaList />},
+];
+
 const Tasks = () => {
   return <div>Tasks</div>
 }
