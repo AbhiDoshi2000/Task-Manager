eccb3891d266b03558fe6a73552788f21fbef030
AbhiDoshi2000 <abhidoshi2000@gmail.com>
2025-01-25 20:04:57 -0600

    ToDo, In Progress & Completed Tabs

--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -4,6 +4,11 @@ import { FaList } from "react-icons/fa";
 import { useParams } from 'react-router-dom';
 import { useState } from 'react';
 import Loading from '../components/Loader';
+import Title from '../components/Title';
+import { IoMdAdd } from 'react-icons/io';
+import { Button } from '@headlessui/react';
+import Tabs from '../components/Tabs';
+import TaskTitle from '../components/TaskTitle';
 
 
 const TABS = [
@@ -13,7 +18,7 @@ const TABS = [
 
 const TASK_TYPE = {
   todo: "bg-blue-600",
-  "in progress": "bg-yello-600",
+  "in progress": "bg-yellow-600",
   completed: "bg-green-600",
 };
 
@@ -24,7 +29,8 @@ const Tasks = () => {
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const status = params?.status || ""
+  const status = params?.status || "";
+
   return loading ? (
     <div className='py-10'>
       <Loading />
@@ -32,7 +38,28 @@ const Tasks = () => {
   ) : (
     <div className='w-full'>
       <div className='flex items-center justify-between mb-4'>
-        <Title title={status ? `${status} Tasks`: "Tasks"} />
+      <Title title={status ? `${status} Tasks` : "Tasks"} />
+
+      {!status && (
+        <Button
+          onClick={() => setOpen(true)}
+          label='Create Task'
+          icon={<IoMdAdd className='text-lg' />}
+          className='flex flex-row-reverse gap-1 items-center bg-blue-600 text-white rounded-md py-2 2xl:py-2.5'
+        />
+      )}
+      </div>
+
+      <div>
+        <Tabs tabs={TABS} setSelected={setSelected}>
+          {!status && (
+            <div className='w-full flex justify-between gap-4 md:gap-x-12 py-4'>
+              <TaskTitle label="To Do" className={TASK_TYPE.todo}/>
+              <TaskTitle label="In Progress" className={TASK_TYPE["in progress"]}/>
+              <TaskTitle label="Completed" className={TASK_TYPE.completed}/>
+            </div>
+          )}
+        </Tabs>
       </div>
     </div>
   );
