cde1c539de88febffcab28b03a70be95da91b909
AbhiDoshi2000 <abhidoshi2000@gmail.com>
2025-01-25 18:09:24 -0600

    continuation of tasks page

--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -5,6 +5,12 @@ const TABS = [
   { title: "List View", icon: <FaList />},
 ];
 
+const TASK_TYPE = {
+  todo: "bg-blue-600",
+  "in progress": "bg-yello-600",
+  completed: "bg-green-600",
+};
+
 const Tasks = () => {
   return <div>Tasks</div>
 }
