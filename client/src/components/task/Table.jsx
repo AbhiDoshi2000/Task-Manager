f6fb75b1302a2ee17caeee9a7b81debf5fbefd32
AbhiDoshi2000 <abhidoshi2000@gmail.com>
2025-01-31 00:31:10 -0600

    Edits done in Table and Tabs

--- client/src/components/task/Table.jsx
+++ client/src/components/task/Table.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import clsx from 'clsx';
+import React, { useState } from 'react'
+import { BiMessageAltDetail } from 'react-icons/bi'
+import {
+    MdAttachFile,
+    MdKeyboardArrowDown,
+    MdKeyboardArrowUp,
+    MdKeyboardDoubleArrowUp,
+} from "react-icons/md";
+import { toast } from 'sonner';
+import { PRIOTITYSTYELS, TASK_TYPE } from '../../utils';
 
-const Table = () => {
+const ICONS = {
+    high: <MdKeyboardDoubleArrowUp />,
+    medium: <MdKeyboardArrowUp />,
+    low: <MdKeyboardArrowDown />,
+};
+
+const Table = ({ tasks }) => {
+    const [openDialog, setOpenDialog] = useState(false);
+    const [selected, setSelected] = useState(null);
+
+    const TableHeader = ()=> (
+        <thead className='border-b border-gray-300'>
+          <tr className='text-black text-left'>
+            <th className='py-2'>Task Title</th>
+            <th className='py-2'>Priority</th>
+            <th className='py-2'>Team</th>
+            <th className='py-2 hidden md:block'>Created At</th>
+          </tr>
+        </thead>
+    );
+
+    const TableRow = ({ task }) => (
+        <tr className='border-b border-gray-200 text-gray-600 hover:bg-gray-300/10'>
+            <td className='py-2'>
+                <div className='flex items-center gap-2'>
+                    <div 
+                        className={clsx("w-4 h-4 rounded-full", TASK_TYPE[task.stage])}
+                    />
+                    <p className='w-full line-clamp-2 text-base text-black'>
+                        {task?.title}
+                    </p>
+                </div>
+            </td>
+
+            <td className='py-2'>
+                <div className='flex gap-1 items-center'>
+                    <span className={clsx("text-lg", PRIOTITYSTYELS[tasks?.priority])}>   
+                        {ICONS[task?.priority]}
+                    </span>
+                    <span className='capitalize line-clamp-1'>
+                        {task?.priority} Priority
+                    </span>
+                </div>
+            </td>
+        </tr>
+    );
+
   return (
-    <div>Table</div>
-  )
-}
+    <>
+      <div className='bg-white px-2 md:px-4 pt-4 pb-9 shadow-md rounded'>
+        <div className='overflow-x-auto'>
+            <table className='w-full'>
+                <TableHeader />
+                    <tbody>
+                        {tasks?.map((task, index)=> (
+                            <TableRow key={index} task={task} />
+                        ))}
+                    </tbody>
+            </table>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Table
--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -11,11 +11,74 @@ import TaskTitle from '../components/TaskTitle';
 import BoardView from '../components/BoardView';
 import { tasks } from '../assets/data';
 import Button from '../components/Button';
+import Table from '../components/task/Table';
 
+
+// const TABS = [
+//   { title: "Board View", icon: <MdGridView />},
+//   { title: "List View", icon: <FaList />},
+// ];
+
+// const TASK_TYPE = {
+//   todo: "bg-blue-600",
+//   "in progress": "bg-yellow-600",
+//   completed: "bg-green-600",
+// };
+
+// const Tasks = () => {
+//   const params = useParams();
+
+//   const [selected, setSelected] = useState(0);
+//   const [open, setOpen] = useState(false);
+//   const [loading, setLoading] = useState(false);
+
+//   const status = params?.status || "";
+
+//   return loading ? (
+//     <div className='py-10'>
+//       <Loading />
+//     </div>
+//   ) : (
+//     <div className='w-full'>
+//       <div className='flex items-center justify-between mb-4'>
+//       <Title title={status ? `${status} Tasks` : "Tasks"} />
+
+//       {!status && (
+//         <Button
+//           onClick={() => setOpen(true)}
+//           label='Create Task'
+//           icon={<IoMdAdd className='text-lg' />}
+//           className='flex flex-row-reverse gap-1 items-center bg-blue-600 text-white rounded-md py-2 2xl:py-2.5'
+//         />
+//       )}
+//       </div>
+
+//       <Tabs tabs={TABS} setSelected={setSelected}>
+//         {!status && (
+//             <div className='w-full flex justify-between gap-4 md:gap-x-12 py-4'>
+//               <TaskTitle label="To Do" className={TASK_TYPE.todo}/>
+//               <TaskTitle label="In Progress" className={TASK_TYPE["in progress"]}/>
+//               <TaskTitle label="Completed" className={TASK_TYPE.completed}/>
+//             </div>
+//         )}
+
+//         {selected !== 1 ? (
+//           <BoardView tasks={tasks} /> 
+//         ): (
+//           <div className='w-full'>
+//             <Table tasks={tasks}/>
+//           </div>
+//         )}
+//       </Tabs>
+//     </div>
+//   );
+// };
+
+// export default Tasks
 
 const TABS = [
-  { title: "Board View", icon: <MdGridView />},
-  { title: "List View", icon: <FaList />},
+  { title: "Board View", icon: <MdGridView /> },
+  { title: "List View", icon: <FaList /> },
 ];
 
 const TASK_TYPE = {
@@ -40,38 +103,42 @@ const Tasks = () => {
   ) : (
     <div className='w-full'>
       <div className='flex items-center justify-between mb-4'>
-      <Title title={status ? `${status} Tasks` : "Tasks"} />
+        <Title title={status ? `${status} Tasks` : "Tasks"} />
 
-      {/* Check on this button, as it doesn't appear on the screen */}
-      {!status && (
-        <Button
-          onClick={() => setOpen(true)}
-          label='Create Task'
-          icon={<IoMdAdd className='text-lg' />}
-          className='flex flex-row-reverse gap-1 items-center bg-blue-600 text-white rounded-md py-2 2xl:py-2.5'
-        />
-      )}
+        {!status && (
+          <Button
+            onClick={() => setOpen(true)}
+            label='Create Task'
+            icon={<IoMdAdd className='text-lg' />}
+            className='flex flex-row-reverse gap-1 items-center bg-blue-600 text-white rounded-md py-2 2xl:py-2.5'
+          />
+        )}
       </div>
 
-      <div>
-        <Tabs tabs={TABS} setSelected={setSelected}>
-          {!status && (
-            <div className='w-full flex justify-between gap-4 md:gap-x-12 py-4'>
-              <TaskTitle label="To Do" className={TASK_TYPE.todo}/>
-              <TaskTitle label="In Progress" className={TASK_TYPE["in progress"]}/>
-              <TaskTitle label="Completed" className={TASK_TYPE.completed}/>
-            </div>
-          )}
+      <Tabs tabs={TABS} setSelected={setSelected}>
+        {!status && (
+          <div className='w-full flex justify-between gap-4 md:gap-x-12 py-4'>
+            <TaskTitle label='To Do' className={TASK_TYPE.todo} />
+            <TaskTitle
+              label='In Progress'
+              className={TASK_TYPE["in progress"]}
+            />
+            <TaskTitle label='completed' className={TASK_TYPE.completed} />
+          </div>
+        )}
 
-          {selected === 0 ? <BoardView tasks={tasks} /
-          > : <div >
-            <Table 
-              task={tasks}
-            /></div>}
-        </Tabs>
-      </div>
+        {selected !== 1 ? (
+          <BoardView tasks={tasks} />
+        ) : (
+          <div className='w-full'>
+            <Table tasks={tasks} />
+          </div>
+        )}
+      </Tabs>
+
+      {/* <AddTask open={open} setOpen={setOpen} /> */}
     </div>
   );
 };
 
-export default Tasks
+export default Tasks;
