7dac95a6f744a44eb8cacea24018a8953f812f99
AbhiDoshi2000 <abhidoshi2000@gmail.com>
2025-01-31 23:13:32 -0600

    Working on Task Details page

--- client/src/pages/TaskDetails.jsx
+++ client/src/pages/TaskDetails.jsx
@@ -1,4 +1,20 @@
-import React from 'react'
+import clsx from 'clsx';
+import moment from 'moment';
+import React from 'react';
+import { FaBug, FaTasks, FaThumbsUp, FaUser } from 'react-icons/fa';
+import {GrInProgress} from 'react-icons/gr';
+import {
+  MdKeyboardArrowDown,
+  MdKeyboardArrowUp,
+  MdKeyboardDoubleArrowUp,
+  MdOutlineDoneAll,
+  MdOutlineMessage,
+  MdTaskAlt,
+} from 'react-icons/md'; 
+import { RxActivityLog } from 'react-icons/rx';
+import { useParams } from 'react-router-dom';
+import { toast } from 'sonner';
+
 
 const TaskDetails = () => {
   return <div>TaskDetails</div>
