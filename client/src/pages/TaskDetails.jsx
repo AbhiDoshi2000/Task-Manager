48e9dcade83e0d5b38170c649cd1a2e797e9f283
AbhiDoshi2000 <abhidoshi2000@gmail.com>
2025-02-04 19:07:01 -0600

    Left side for task details done

--- client/src/pages/TaskDetails.jsx
+++ client/src/pages/TaskDetails.jsx
@@ -17,6 +17,7 @@ import { useParams } from 'react-router-dom';
 import { toast } from 'sonner';
 import { tasks } from '../assets/data';
 import Tabs from '../components/Tabs';
+import { getInitials, PRIOTITYSTYELS, TASK_TYPE } from '../utils';
 
 
 const assets = [
@@ -86,7 +87,7 @@ const act_types = [
 ];
 
 const TaskDetails = () => {
-  const {id} = useParams()
+  const { id } = useParams()
 
   const [selected, setSelected] = useState(0)
   const task = tasks[0]
@@ -96,16 +97,114 @@ const TaskDetails = () => {
       <h1 className='text-2xl text-gray-600 font-bold'>{task?.title}</h1>
 
       <Tabs tabs={TABS} setSelected={setSelected}>
-        {selected === 0 ? <>
-          <div className='w-full flex flex-col md:flex-row gap-5 2xl:gap-8 bg-white shadow-md p-8 overflow-y-auto'>
-            <div className='w-full md:w-1/2 space -y-8'>
-              <div className='flex items-center gap-5'>
+        {selected === 0 ? ( 
+          <>
+            <div className='w-full flex flex-col md:flex-row gap-5 2xl:gap-8 bg-white shadow-md p-8 overflow-y-auto'>
+              {/* Left side */}
+              <div className='w-full md:w-1/2 space-y-8'>
+                <div className='flex items-center gap-5'>
+                  <div
+                    className={clsx(
+                      "flex gap-1 items-center text-base font-semibold px-3 py-1 rounded-full",
+                      PRIOTITYSTYELS[task?.priority],
+                      bgColor[task?.priority]
+                    )}
+                  >
+                    <span className='text-lg'>{ICONS[task?.priority]}</span>
+                    <span className='uppercase'>{task?.priority} Priority</span>
+                  </div>
 
+                  <div className={clsx("flex items-center gap-2")}>
+                    <div className={clsx("w-4 h-4 rounded-full", TASK_TYPE[task.stage])}/>
+                      <span className='text-black uppercase'>{task?.stage}</span>
+                  </div>   
+                </div>
+
+                <p className='text-gray-500'>
+                    Created At: {new Date(task?.date).toDateString()}
+                </p>
+
+                <div className='flex items-center gap-8 p-4 border-y border-gray-200'>
+                  <div className='space-x-2'>
+                    <span className='font-semibold'>Assests :</span>
+                    <span>{task?.assets?.length}</span>
+                  </div> 
+
+                  <span className='text-gray-400'>|</span>
+                  
+                  <div className='space-x-2'>
+                    <span className='font-semibold'>Sub-Task :</span>
+                    <span>{task?.subTasks?.length}</span>
+                  </div>
+                </div>
+
+                <div className='space-y-4 py-6'> 
+                  <p className='text-gray-600 font-semibold text-sm'>
+                    TASK TEAM
+                  </p>
+                  <div className='space-y-3'>
+                    {task?.team?.map((m ,index) => (
+                      <div
+                        key={index}
+                        className='flex gap-4 py-2 items-center border-t border-gray-200'
+                      >
+                        <div
+                          className={
+                            "w-10 h-10 rounded-full text-white flex items-center justify-center text-sm -mr-1 bg-blue-600"
+                          }
+                        >
+                          <span className='text-center'>
+                            {getInitials(m?.name)}
+                          </span>
+                        </div>
+
+                        <div>
+                          <p className='text-lg font-semibold'>{m?.name}</p>
+                          <span className='text-gray-500'>{m?.title}</span>
+                        </div> 
+                      </div>
+                    ))}
+                  </div>
+                </div>
+
+                <div className='space-y-4 py-6'>
+                  <p className='text-gray-500 font-semibold text-sm'>
+                    SUB-TASKS
+                  </p>
+                  <div className='space-y-8'>
+                    {task?.subTasks.map((el, index) => (
+                      <div key={index} className='flex gap-3'>
+                        <div>
+                          <MdTaskAlt className='text-violet-600' size={26} /> 
+                        </div>
+
+                        <div className='space-y-1'>
+                          <div className='flex gap-2 items-center'> 
+                            <span className='text-sm text-gray-500'>
+                              {new Date(el?.date).toDateString()}
+                            </span>
+
+                            <span className='px-2 py-0.5 text-center text-sm rounded-full bg-violet-100 text-violet-700 font-semibold'>
+                              {el?.tag}
+                            </span>
+                          </div>
+
+                          <p className='text-gray-700'>{el?.title}</p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>  
+              </div>
+              {/* Right side */}
+              <div className='w-full md:w-1/2 space-y-8'>
+                    
               </div>
             </div>
-            <div className='w-full md:w-1/2 space -y-8'></div>
-          </div>
-        </> : <></>}
+          </> 
+        ) : ( 
+          <></>
+        )}
       </Tabs>
     </div>
   );
