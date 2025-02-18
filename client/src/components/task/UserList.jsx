9b7e0447147d969c12771b030d56d7eea43b73a1
AbhiDoshi2000 <abhidoshi2000@gmail.com>
2025-02-18 01:08:28 -0600

    Completion of Add Task

--- client/src/components/task/UserList.jsx
+++ client/src/components/task/UserList.jsx
@@ -1,9 +1,10 @@
 import { summary } from '../../assets/data'
-import { Listbox, ListboxButton, ListboxOptions, Transition } from "@headlessui/react";
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions, Transition } from "@headlessui/react";
 import { Fragment, useEffect, useState } from "react";
 import { BsChevronExpand } from "react-icons/bs";
 import { MdCheck } from "react-icons/md"; 
-
+import clsx from 'clsx'
+import { getInitials } from "../../utils";
 
 const UserList = ({ setTeam, team }) => {
     const data = summary.users;
@@ -13,6 +14,14 @@ const UserList = ({ setTeam, team }) => {
         setSelectedUsers(el)
         setTeam(el.map((u) => u._id));
     };
+
+    useEffect(() => {
+        if (team?.length < 1) {
+            data && setSelectedUsers([data[0]]);
+        } else {
+            setSelectedUsers(team);
+        }
+    }, [])
 
   return (
     <div>
@@ -46,7 +55,35 @@ const UserList = ({ setTeam, team }) => {
                 >
 
                     <ListboxOptions className='z-50 absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm'>
-
+                        {data.map((user, index) => (
+                            <ListboxOption
+                                key={index}
+                                className={({ active }) => 
+                                    `relative cursor-default select-none py-2 pl-10 pr-4. ${
+                                    active ? "bg-amber-100 text-amber-900" : "text-gray-900"
+                                    }`
+                                }
+                                value={user}
+                            >
+                                {({selected}) => (
+                                    <>
+                                        <div className={clsx("flex items-center gap-2 truncate", selected ? "font-medium" : "font-normal")}>
+                                            <div className='w-6 h-6 rounded-full text-white flex items-center justify-center bg-violet-600'>
+                                                <span className='text-center text-[10px]'>
+                                                    {getInitials(user.name)}
+                                                </span>
+                                            </div>
+                                            <span>{user.name}</span>
+                                        </div>
+                                        {selected ? (
+                                            <span className='absolute inset-y-0 left-0 flex items-ceter pl-3 text-amber-600'>
+                                                <MdCheck className='h-5 w-5' aria-hidden='true' />
+                                            </span>
+                                        ) : null}
+                                    </>
+                                )}
+                            </ListboxOption>
+                        ))}
                     </ListboxOptions>
                 </Transition>
             </div>
